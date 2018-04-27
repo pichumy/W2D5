c434a581986dddb8885afd8a8873368dc95f256e
Dan Luo <pichumy@gmail.com>
2018-04-27 15:08:18 -0700

    pass rspecs for IntSet and Hashing

--- MyHashMap/lib/p01_int_set.rb
+++ MyHashMap/lib/p01_int_set.rb
@@ -1,22 +1,32 @@
+require 'byebug'
 class MaxIntSet
+  attr_accessor :store
   def initialize(max)
+    @max = max
+    @store = Array.new(max) { false }
   end
 
   def insert(num)
+    validate!(num)
+    store[num] = true 
   end
 
   def remove(num)
+    store[num] = false if is_valid?(num)
   end
 
   def include?(num)
+    store[num] if is_valid?(num)
   end
 
   private
 
   def is_valid?(num)
+    num < @max  && num > 0 
   end
 
   def validate!(num)
+    raise "Out of bounds" unless is_valid?(num)
   end
 end
 
@@ -24,20 +34,27 @@ end
 class IntSet
   def initialize(num_buckets = 20)
     @store = Array.new(num_buckets) { Array.new }
+    @max = num_buckets
   end
 
   def insert(num)
+    self[num].push(num) 
   end
-
+  
   def remove(num)
+    idx = self[num].index(num)
+    self[num].delete_at(idx)
   end
 
   def include?(num)
-  end
+    self[num].include?(num)
+  end 
 
   private
 
   def [](num)
+    key = num % @max 
+    @store[key]
     # optional but useful; return the bucket corresponding to `num`
   end
 
@@ -52,21 +69,35 @@ class ResizingIntSet
   def initialize(num_buckets = 20)
     @store = Array.new(num_buckets) { Array.new }
     @count = 0
+    @num_buckets = num_buckets
   end
 
   def insert(num)
+    if @count == num_buckets / 2 + 1  
+      resize!
+    end
+    unless include?(num)
+      @count += 1 
+      self[num].push(num) 
+    end 
   end
 
   def remove(num)
+    if include?(num)
+      self[num].delete(num)
+      @count -= 1
+    end 
   end
 
   def include?(num)
+    self[num].include?(num)
   end
 
   private
 
   def [](num)
-    # optional but useful; return the bucket corresponding to `num`
+    key = num % num_buckets
+    @store[key]
   end
 
   def num_buckets
@@ -74,5 +105,15 @@ class ResizingIntSet
   end
 
   def resize!
+    @num_buckets = @num_buckets * 2 
+    old_store = @store.dup 
+    @store = Array.new(@num_buckets) { Array.new }
+    @count = 0
+    old_store.each do |bucket|
+      bucket.each do |el|
+        self.insert(el)
+      end 
+    end 
+    
   end
 end
--- MyHashMap/lib/p02_hashing.rb
+++ MyHashMap/lib/p02_hashing.rb
@@ -1,14 +1,27 @@
+require 'pry'
+require 'byebug'
+
 class Fixnum
   # Fixnum#hash already implemented for you
 end
 
 class Array
   def hash
+    string = []
+    self.each do |el| 
+      hashed = el.hash 
+      if hashed < 0 
+        hashed *= -1
+      end
+      string << hashed
+    end 
+    string.join.to_i.hash
   end
 end
 
 class String
   def hash
+    self.chars.map { |char| char.ord }.join.to_i.hash 
   end
 end
 
@@ -16,6 +29,12 @@ class Hash
   # This returns 0 because rspec will break if it returns nil
   # Make sure to implement an actual Hash#hash method
   def hash
-    0
+    return 0.hash if self.empty?
+    final_hash = nil
+    self.each do |k, v| 
+      combined_hash = (k.hash.to_s + v.hash.to_s).to_i
+      final_hash = final_hash.nil? ? combined_hash : final_hash ^ combined_hash
+    end 
+    final_hash
   end
 end
